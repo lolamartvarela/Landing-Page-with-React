3f15af4171e534bfda28ddd40a9ad8e8299a2a8a
lolamartvarela <luciamvarela@outlook.com>
2023-01-06 15:35:04 +0000

    Landing Page with React

---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Navbar = function(){
+    return (<nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#"><strong>Start Boostrap</strong></a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="#"><strong>Home</strong></a>
+        <a className="nav-link" href="#">About</a>
+        <a className="nav-link" href="#">Srvice</a>
+        <a className="nav-link disabled">Contact</a>
+      </div>
+    </div>
+  </div>
+</nav>
+            )
+}
+
+export default Navbar 
